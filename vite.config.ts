ff3e97f1a39c65c8e1a56b74ef1fa17ef148cfde
Gabriel Paiva <paivadepaiva00@gmail.com>
2022-08-17 15:25:23 -0300

    chore: added vite.congig

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       // home: resolve(__dirname, 'src/pages/Home.tsx')
-  //     }
-  //   }
-  // }
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
