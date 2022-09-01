cc4415be0fe7d8298a8ebe8c750ae1c637a30461
Gabriel Paiva <gabriel.paiva0@icloud.com>
2022-09-01 17:25:03 -0300

    chore: removed notification

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames'
 import { Avatar, Dropdown } from 'flowbite-react'
-import { Bell, Notification } from 'phosphor-react'
+import { Bell, Notification, SignOut } from 'phosphor-react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { DEFAULT_ICON_SIZE } from '../common/iconSizes'
 import { useAuth } from '../hooks/useAuth'
 
 interface DashboardHeaderProps {}
@@ -43,21 +44,6 @@ export default function DashboardHeader(props: DashboardHeaderProps) {
       <div className="flex flex-1 gap-8 justify-end items-center">
         <Dropdown
           label={
-            <Bell
-              size={28}
-              weight="fill"
-              className="text-zinc-700 hover:text-primary transition-colors"
-            />
-          }
-          arrowIcon={false}
-          inline={true}
-        >
-          <Dropdown.Item>Notificação 1</Dropdown.Item>
-          <Dropdown.Item>Notificação 2</Dropdown.Item>
-        </Dropdown>
-
-        <Dropdown
-          label={
             <Avatar
               alt="Configurações"
               img="/src/assets/ju-logo.png"
@@ -68,10 +54,23 @@ export default function DashboardHeader(props: DashboardHeaderProps) {
           arrowIcon={false}
           inline={true}
         >
-          <Dropdown.Item>Configurações</Dropdown.Item>
-          <Dropdown.Item>Conta</Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => alert('Funcionalidade ainda não implementada')}
+          >
+            Configurações
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => alert('Funcionalidade ainda não implementada')}
+          >
+            Conta
+          </Dropdown.Item>
           <Dropdown.Divider />
-          <Dropdown.Item onClick={logout}>Sair</Dropdown.Item>
+          <Dropdown.Item onClick={logout}>
+            <span className="text-red-500 flex gap-2">
+              <SignOut size={DEFAULT_ICON_SIZE} />
+              Sair
+            </span>
+          </Dropdown.Item>
         </Dropdown>
       </div>
     </div>
